710445d4b533d574f6eaf322726d8eb56a65d341
SeongJin Ahn <sjahn@astams.com>
2018-08-17 20:27:20 +0900

    [encoding] set default code page and encoding to us.utf-8

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ R.prototype.call = function (_opts, _callback, resolve) {
   var callback = _callback || _opts;
   var opts = _.isFunction(_opts) ? {} : _opts;
   this.options.env.input = JSON.stringify([this.d, this.path, opts]);
+  this.options.env.LANG = "en_US.UTF-8";
   var child = child_process.spawn("Rscript", this.args, this.options);
   child.stderr.on("data", callback);
   //-->
